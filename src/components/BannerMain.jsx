56cfd48522aede06bf0b474761f18ccc72cbf270
Guilherme Henrique <gh.dourado77@gmail.com>
2023-10-24 08:25:52 -0300

    fix: trocando bg

--- src/components/BannerMain.jsx
+++ src/components/BannerMain.jsx
@@ -1,22 +1,16 @@
 export function BannerMain() {
   return (
-    <div className="bg-[url('/bg.webp')] bg-cover w-full">
-      <div className="bg-[url('/ale.png')] bg-no-repeat bg-bottom ">
-        <div className="bg-black/40 px-4 pt-72 pb-12 w-full">
-          <div className="max-w-6xl mx-auto">
-            <h2 className="text-xl sm:text-4xl font-bold text-white text-center pb-10">
-              Transforme seu corpo, supere seus limites e conquiste resultados
-              incríveis com treinos exclusivos no perfil privado <br />
-              @AlêTreinos
-            </h2>
-            <div className="text-white text-center space-y-3 max-w-lg mx-auto">
-              <button className="bg-black text-white w-full py-4 rounded-md font-medium uppercase">
-                Quero treinar com Alessandra
-              </button>
-              <p>
-                A partir de <strong>R$26,67/mês</strong> no plano anual
-              </p>
-            </div>
+    <div className="bg-[url('/banner.png')] bg-no-repeat bg-cover bg-right md:bg-top relative">
+      <div className="absolute bg-[#5E0583] h-4 bottom-0 w-full blur-3xl" />
+      <div className="px-4 h-screen w-full">
+        <div className="text-white text-center max-w-6xl mx-auto h-full flex items-end justify-end">
+          <div className="space-y-3 pb-12">
+            <button className="bg-violet-700 hover:bg-violet-600 duration-300 text-white w-full py-2 sm:py-4 text-sm sm:text-base px-24 rounded-md font-medium uppercase">
+              Quero treinar com Alessandra
+            </button>
+            <p>
+              A partir de <strong>R$26,67/mês</strong> no plano anual
+            </p>
           </div>
         </div>
       </div>
