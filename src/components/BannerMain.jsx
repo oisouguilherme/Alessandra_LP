bdbabb6682c8ed01654af8599367ca32fdc45064
Guilherme Henrique <gh.dourado77@gmail.com>
2024-01-25 11:10:25 -0300

    feat: add button no main

--- src/components/BannerMain.jsx
+++ src/components/BannerMain.jsx
@@ -1,17 +1,25 @@
 import Image from "next/image";
-import { Link } from "react-scroll";
 
 export function BannerMain() {
   return (
     <div className="bg-[url('/bannerMobile.png')] md:bg-[url('/banner2.png')] bg-no-repeat bg-cover bg-center  relative">
-      <Link to="comprar" smooth={true}>
+      <div className="relative">
         <Image
           src={"/bannerMobile.png"}
           width={800}
           height={800}
           className="w-full md:hidden"
         />
-      </Link>
+        <div className="flex justify-center">
+          <a
+            href="https://pay.hotmart.com/M81294188Q?off=u9i14bx1&bid=1700597420698"
+            target="_blank"
+            className="bg-green-500 hover:bg-green-400 duration-300 text-white w-[80%] py-4 text-sm sm:text-base rounded-md font-medium uppercase absolute bottom-12 text-center"
+          >
+            QUERO TREINAR COM A ALÊ
+          </a>
+        </div>
+      </div>
       <div className="px-4 h-screen w-full hidden md:block">
         <div className="text-white text-center max-w-6xl mx-auto h-full flex items-end justify-end">
           <div
